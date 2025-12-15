478a2f155a912a432fa8a0727b440fe3684e4c63
jaredvaldez-4 <jared.valdez@csuglobal.edu>
2025-12-15 00:03:49 -0700

    Add files via upload

---
+++ Program2_CALC.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 14 23:45:22 2025
+
+@author: jared
+"""
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+multiplX = num1 * num2
+
+if num2 != 0:
+    divsX = num1 / num2
+    print("Division result from the first numbert:",  divsX)
+else:
+    print("WARNING: Division BY 0 NOT allowed please try again.")
+
+print("Multiplication result:", multiplX)
--- Program_1_CALC.py
+++ Program_1_CALC.py
@@ -8,8 +8,8 @@ Created on Sun Dec 14 23:43:59 2025
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-addition = num1 + num2
-subtraction = num1 - num2
+addnownw = num1 + num2
+subtractnw = num1 - num2
 
-print("Addition Results from the two numbers:", addition)
-print("Subtraction Result from the two numbers:", subtraction)
+print("Addition Results from the two numbers:", addnownw )
+print("Subtraction Result from the two numbers:", subtractnw)
