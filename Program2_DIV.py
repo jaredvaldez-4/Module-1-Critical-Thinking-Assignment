b70ad7cf6f65e0222269174f23c3a1347977207f
jaredvaldez-4 <jared.valdez@csuglobal.edu>
2025-12-15 00:09:45 -0700

    Update Program2_DIV.py

--- Program2_DIV.py
+++ Program2_DIV.py
@@ -8,12 +8,13 @@ Created on Sun Dec 14 23:45:22 2025
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-multiplication = num1 * num2
+multiplX = num1 * num2
 
 if num2 != 0:
-    division = num1 / num2
-    print("Division result from the first numbert:", division)
+    divsX = num1 / num2
+    print("Division result from the first numbert:",  divsX)
 else:
-    print("Division by zero is not allowed please try again.")
+    print("WARNING: Division BY 0 NOT allowed please try again.")
 
-print("Multiplication result:", multiplication)
+print("Multiplication result:", multiplX)
+
